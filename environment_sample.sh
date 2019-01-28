72bd15132fcb18edede255a9968b0d070c66d80c
Peter Burkholder <peter.burkholder@gsa.gov>
2019-01-28 12:43:41 -0500

    Add portability
    nginx.conf: Replaced hardcoded resolver with first one found from `/etc/resolv.conf`
    (cripped from https://github.com/cdelashmutt-pivotal/nginx-route-service)
    
    manifest.yml.erb: Defined `env` var so it won't pick up any `.` in the env name
    when setting up routes.  Update to new `buildpacks` notation.
    
    environment_sample.sh: Add more boilerplate as comments, and allow all IPV4
    to start with
    
    Makefile: export all variables needed for `erb` template generations
    
    .gitignore: Ignore sample `manifest.yml` files

--- environment_sample.sh
+++ environment_sample.sh
@@ -1,2 +1,5 @@
-export ENV=
-export IP_WHITELIST=
+export ENV=     
+export IP_WHITELIST=0.0.0.0/0 # example: allow all ipv4 addresses
+# export PAAS_ORG=gsa-sandbox
+# export PAAS_APP_NAME=re-ip-whitelist-service
+# export PAAS_DOMAIN=app.cloud.gov
